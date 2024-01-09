b48d97887dde19e3bab5a8fb71a16906359617ca
Samuel-Ricco <samuelricco@gmail.com>
2024-01-09 10:40:10 +0100

    add product name in grind on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,31 @@
 import Image from 'next/image'
 import Container from './components/container/Container'
 import HomeBanner from './components/home/HomeBanner'
+import { products } from './components/product/products'
+import { TextCutter } from '@/utils/TextCutter'
 
 export default function Home() {
   return (
     <div className='p-8'>
       <Container>
         <div>
-          <HomeBanner>
+          <HomeBanner/>
+        </div>
 
-          </HomeBanner>
+        <div className='
+          grid
+          grid-cols-2
+          sm:grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8'
+        >
+          {products.map((product:any)=>{
+            return <div>
+              {TextCutter(product.name)}
+            </div>
+          })}
         </div>
       </Container>
     </div>

a2b1c38f82f2c092ebecca866ff26c98251134d4
Samuel-Ricco <samuelricco@gmail.com>
2024-01-10 12:30:13 +0100

    product fix

---
+++ model/Product.ts
@@ -0,0 +1,33 @@
+class Product {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  brand: string;
+  category: string;
+  inStock: boolean;
+  images: ProductImage[];
+  reviews: Review[] | undefined;
+
+  constructor(
+    id: string,
+    name: string,
+    description: string,
+    price: number,
+    brand: string,
+    category: string,
+    inStock: boolean,
+    images: ProductImage[]
+  ) {
+    (this.id = id),
+      (this.name = name),
+      (this.description = description),
+      (this.price = price),
+      (this.brand = brand),
+      (this.category = category),
+      (this.inStock = inStock),
+      (this.images = images);
+  }
+
+  
+}
